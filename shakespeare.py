a70cfb2db1bf814dbba28e16562425c9b05f89ff
williamclift <cliftjwr@gmail.com>
2020-08-16 14:25:53 -0400

    ngrams

--- shakespeare.py
+++ shakespeare.py
@@ -2,10 +2,37 @@
 	Shakespeare Machine Learning Algorithm
 
 		@author William Clift
-			- 1 August 2020
+			- 16 August 2020
 
 '''
 
-#EDIT 1
+def oneGram(str):
+	return str.split(' ')
 
-#EDIT 2
+def nGram(tokens, n):
+	newList = list()
+	num = len(tokens)
+
+	for i in range(num - n):
+		concat = ''
+		for j in range(n):
+			concat += tokens[i+j] + ' '
+
+		newList.append(concat.rstrip())
+
+	return newList
+
+
+
+#function to split text into word
+tokens = oneGram("The quick brown fox jumps over the lazy dog")
+
+for token in tokens:
+	print(token)
+
+for e in nGram(tokens, 3):
+	print(e)
+
+
+
+
